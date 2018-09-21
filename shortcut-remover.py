70d8c18fcdb175adf3c3cd4ff51c6e52982e496b
Abdullah Ahmed <elmagicabdulah@gmail.com>
2018-09-21 23:28:12 +0200

    Update shortcut-remover.py

--- shortcut-remover.py
+++ shortcut-remover.py
@@ -17,27 +17,28 @@ shortcuts = 0
 empty_folders = 0
 
 def scan(path):
-	global shortcuts, empty_folders
+	# This function scans for all files in a path
+	global shortcuts, empty_folders # variables for the number of shortcuts and empty folders
 
-	if os.path.isfile(path):
-		if os.path.splitext(path)[1].lower() == ".lnk":
+	if os.path.isfile(path): 
+		if os.path.splitext(path)[1].lower() == ".lnk": # if the file's extension is .ink, it means it's a shortcut.. so remove this file
 			shortcuts += 1
 			print("{} is a shortucut virus. removed.".format(os.path.basename(path)))
 			os.remove(path)
 	else:
-		if len(os.listdir(path)) < 1:
-			try:
+		if len(os.listdir(path)) < 1: # if the folder doen't have any file
+			try: # to remove it
 				empty_folders += 1
 				print("{} is an empty folder. removed.".format(path))
 				shutil.rmtree(path)
-			except:
+			except: # you don't have the permissions to remove it
 				print("can't remove {}".format(path))
-		else:
+		else: # if the folder has files inside it
 			for arg in os.listdir(path):
-				scan(path + "/" + arg)
+				scan(path + "/" + arg) # scan them
 
-for partition in os.listdir(os.getcwd()):
+for partition in os.listdir(os.getcwd()): # scan all partitions
 	scan(partition)
 
 
-print("\nFinished!\n\nResults\n------------\nDeleted shortcuts: {}\nDeleted empty folders: {}".format(shortcuts, empty_folders))
+print("\nFinished!\n\nResults\n------------\nDeleted shortcuts: {}\nDeleted empty folders: {}".format(shortcuts, empty_folders)) # show final output
